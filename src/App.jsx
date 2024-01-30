de79dcbf02be8f1ca85f4ec8ab65f3ad494f406e
JORDI\Jordi <jordi_martinezjimenez@iescarlesvallbona.cat>
2024-01-30 12:36:41 +0100

    First steps using Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+
+const Contenedor = styled.div`
+  max-width:900px;
+  margin: 0 auto;
+  width: 90;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Imagen = styled.img`
+  max-width:400px;
+  margin: 100px auto 0 auto;
+  width: 80%;
+  display: block;
+`
+
+const Heading = styled.h1`
+  font-family: 'lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
 
 function App() {
 
   return (
-    <h1>Desde App</h1>
+    <Contenedor>
+      <Imagen
+        src={ImagenCripto}
+        alt="Imagenes Criptomonedas"
+      />
+      <Heading>Cotiza Criptomonedas al instante</Heading>
+    </Contenedor>
   )
 }
 
